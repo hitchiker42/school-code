0c92664244def09dc05e9207f261335d0a68559d
Tucker DiNapoli <Hitchiker42@gmail.com>
2013-01-29 09:28:26 -0500

    some modifications to simple iam751 c files

--- iam751/hello.c
+++ iam751/hello.c
@@ -3,5 +3,10 @@
 int main (int argc, char **argv)
 {
   greeting();
-  printf("10 factorial is %d\n", factorial(10));
+  if (argc==1 || atoi(argv[1])==0) {
+    printf("10 factorial is %d\n", factorial(10));
+  } else {
+      int num=atoi(argv[1]);
+      printf("%d factorial is %d\n", num,factorial(num));
+  }
 }
