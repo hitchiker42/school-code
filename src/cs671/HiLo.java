0356e885ee5c03dc3eaf8b311964a9cab149cdf5
tpm29 <tpm29@6c1987e8-742f-4adb-b027-1fd7972a12de>
2013-02-04 12:58:37 +0000

    finished HiLo class, Still need to test and document it though
    git-svn-id: https://stsvn.cs.unh.edu/svn/cs671.tpm29@7 6c1987e8-742f-4adb-b027-1fd7972a12de

--- src/cs671/HiLo.java
+++ src/cs671/HiLo.java
@@ -1,40 +1,60 @@
 package cs671;
 //NEEDS TO BE FIXED, SPECIFICALLY THE HAS SOLVED METHOD
+
+/**
+   HiLo Class
+ */
 public class HiLo implements Guesser<Integer> {
-    int min,max,x;
-    double total;
-    public HiLo(int min,int max){
-        this.max=max;
-        this.min=min;
-        this.x=(min+max)/2;
-        this.total=(double)(max-min);
+  int min,max,x;
+  int range_min,range_max;
+  double total;
+  Range range;
+  static class Range {
+    static int min;
+    static int max;
+    Range(int min,int max){
+      this.min=min;
+      this.max=max;
     }
-    public Integer getSecret(){
-        return x;
+  }
+  public HiLo(int min,int max){
+    this.max=max;
+    this.min=min;
+    range= new Range(min,max);
+    this.x=(min+max)/2;
+    this.total=(double)(max-min);
+  }
+  public Integer getSecret(){
+    return x;
+  }
+  public boolean hasSolved(){
+    //System.out.println(String.format("min:%d,max:%d",min,max));
+    if (this.min == this.max){
+      x=min;
+      assert(x==max);
+      return true;
+    } else {
+      return false;
     }
-    public boolean hasSolved(){
-        if (min == max){
-            return true;
-        } else {
-            return false;
-        }
-    }
-    public String initalize(){
-        return "";
-    }
-    public String makeQuestion(){
-        x=(min + max)/2; //might not work like this, b/c of rounding
-        String question=String.format("Is your number greater than %d",x);
-        return question;
-    }
-    public void no(){
-        this.max=this.x;
-    }
-    public void yes(){
-        this.min=this.x;
-    }
-    public double progress(){
-        Double current=(double)(max-min);
-        return(1-current/total);
-    }
+  }
+  public String initialize(){
+    max=range.max;
+    min=range.min;
+    return "";
+  }
+  public String makeQuestion(){
+    x=((min + max)/2+(min + max)%2); //might not work like this, b/c of rounding
+    String question=String.format("Is your number greater than or equal to %d",x);
+    return question;
+  }
+  public void no(){
+    this.max=(x-1);
+  }
+  public void yes(){
+    this.min=x;
+  }
+  public double progress(){
+    Double current=(double)(max-min);
+    return(1-current/total);
+  }
 }
